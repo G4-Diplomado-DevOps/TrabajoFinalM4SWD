5aaad0a693732d685de89f63bb9bc48ac3e5c5c2
José Ibañez <jibanez123456@gmail.com>
2021-01-30 14:25:36 -0300

    Agregar Calculo de Impuesto

--- src/main/java/com/devops/dxc/devops/model/Util.java
+++ src/main/java/com/devops/dxc/devops/model/Util.java
@@ -42,8 +42,35 @@ public class Util {
 
     public static int getImpuesto(int sueldo) {
 
-        /* 0 impuesto para TODOS */
-        return 0;
+        /* segun la table del SII, impuesto de segunda categoria y condición > 30 UTA (1.530.000 aprox)
+        Tramo de remuneración: $0 - $500.000 | Retiro promedio de la persona afiliada: $989.413 | Impuesto a cancelar: $0
+        TR: $500.000 - $1.000.000 | RP $1.490.241 | IMP. $0
+        TR: $1.000.000 - $1.500.000 | RP: $1.989.013 | IMP: $0
+        TR: $1.500.000 - $2.500.000 | RP: $2.247.661 | IMP: $174.531
+        TR: $2.500.000 - 3.000.000 | RP: $3.815.984 | IMP: $364.086
+        TR: $3.000.000 - $4.000.000 | RP: $3.815.984 | IMP: $612.396
+        TR: $4.000.000 - $5.000.000 | RP: $3.815.984 | IMP: 980.357
+        TR: $5.000.000 - $6.000.000 | RP: $3.815.984 | IMP: 1.294.052
+        TR: $6.000.000 y más | RP: $3.815.984 | IMP: $1.491.042
+         */
+        if (sueldo < 1500000) {
+            return 0;
+        } else if (sueldo > 1500000 && sueldo <= 2500000) {
+            return 174531;
+        } else if (sueldo > 2500000 && sueldo <= 3000000) {
+            return 364086;
+        } else if (sueldo > 3000000 && sueldo <= 4000000) {
+            return 612396;
+        } else if (sueldo > 4000000 && sueldo <= 5000000) {
+            return 980357;
+        } else if (sueldo > 5000000 && sueldo <= 6000000) {
+            return 1294052;
+        } else if (sueldo > 6000000) {
+            return 1491042;
+        } else {
+            return -1;
+        }
+
     }
     
 }
